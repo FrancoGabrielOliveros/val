3c636ce17e71d70a927d5cd1e8cf5db1a06f5eb6
FrancoOliveros <pfrancogabriel26@gmail.com>
2025-01-29 15:17:52 +0800

    Changed TP sensitivity

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ const body = document.body;
 const noButton = body.querySelector(".no");
 
 noButton.addEventListener("click", function () {
-    noButton.style.right = `${generateRandom(70)}dvw`;
+    noButton.style.right = `${generateRandom(60)}dvw`;
     noButton.style.bottom = `${generateRandom(80)}dvh`;
 });
