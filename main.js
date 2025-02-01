217ecfdc6873519393eba78b55853873fca110e1
FrancoOliveros <pfrancogabriel26@gmail.com>
2025-02-02 01:45:58 +0800

    Added Flower Page

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function generateRandom(max) {
 
 const body = document.body;
 const noButton = body.querySelector(".no");
-
+const yesButton = body.querySelector(".yes");
 noButton.addEventListener("click", function () {
     noButton.style.right = `${generateRandom(60)}dvw`;
     noButton.style.bottom = `${generateRandom(80)}dvh`;
